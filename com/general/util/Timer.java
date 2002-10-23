dd29c696cd795fc910d9907b4b84ef105a186b66
gmarceau <>
2002-10-23 19:06:43 +0000

    indents

--- com/general/util/Timer.java
+++ com/general/util/Timer.java
@@ -5,7 +5,7 @@ package com.general.util;
 
 
 /**
-
+	 
 *	Is a classic timer object.. Similar in functionality to the javascript setTimer method.
 
 *	NOTE: This is a loose timer. Events are dispatched wheever they can be after the minum time
@@ -19,145 +19,145 @@ package com.general.util;
 
 
 public class Timer implements Comparable {
-
+	
 	Runnable runnable;
-
+	
 	long time;
-
+	
 	boolean runAsThread;
-
 	
-
+	
+	
 	public Timer(Runnable thingToRun, long timeToWait) {
-
-		this(thingToRun, timeToWait, false);
-
-	}
-
-	
-
-	/**
-
-	*	Run as thread means that the timer should launch the runnable item in its own thread.
-
-	*	Items are not run as threa dby default. Items not run as thread should return ASAP.
-
-	*
-
-	*		Time to wait is relative to the current time ad is in millis.
-
-	*/
-
-	public Timer(Runnable thingToRun, long timeToWait, boolean runAsThread) {
-
-		runnable=thingToRun;
-
-		timeToWait=(timeToWait<=0?1:timeToWait); //assert positive.
-
-		time=System.currentTimeMillis()+timeToWait;
-
-		this.runAsThread=runAsThread;
-
 		
-
-		addEvent(this);
-
+		this(thingToRun, timeToWait, false);
+		
 	}
-
 	
-
-	public long timeLeft() { return time - System.currentTimeMillis(); }
-
-	public boolean isReady() { return time - System.currentTimeMillis() - 1 <= 0; }
-
-
-
-	public int compareTo(Object other) {
-
-		Timer otherTimer = (Timer)other;
-
-		if (time < otherTimer.time) return -1;
-
-		else if (time == otherTimer.time) return 0;
-
-		else return 1;
-
-	}
-
 	
-
+	
 	/**
-
-	*	Called by dispatcher thread to start event.
-
-	*
-
-	*/ 
-
-	private void doEvent() {
-
-		if (runAsThread) {
-
-			(new Thread(runnable)).start();
-
-		} else {
-
-			try {
-
-				runnable.run();
-
-			} catch (Exception ex) {
-
-				ex.printStackTrace();
-
-			}
-
-		}
-
-	}
-
+		 
+	*	Run as thread means that the timer should launch the runnable item in its own thread.
 	
-
-
-
-	static private Semaphore semaphore = new Semaphore(0);
-
-	static private MinHeap timers = new MinHeap();
-
-	static private Thread thread;
-
-
-
-	static private void addEvent(Timer timer) {
-
-		synchronized (timers) {
-
-			timers.add(timer);
-
-			timers.notify(); //instead of sem.getLock(time);
-
-
-
-			if (thread == null)  { 
-
-				thread = (new Thread() {
-
-					public void run() {
-
-						try {timerLoop();} catch (Exception ex) { ex.printStackTrace(); }
-
-					}
-
-				});
-
+	*	Items are not run as threa dby default. Items not run as thread should return ASAP.
+	
+	*
+	
+	*		Time to wait is relative to the current time ad is in millis.
+	
+	*/
+	
+	public Timer(Runnable thingToRun, long timeToWait, boolean runAsThread) {
+		
+		runnable=thingToRun;
+		
+		timeToWait=(timeToWait<=0?1:timeToWait); //assert positive.
+		
+		time=System.currentTimeMillis()+timeToWait;
+		
+		this.runAsThread=runAsThread;
+		
+		
+		
+		addEvent(this);
+		
+	}
+	
+	
+	
+	public long timeLeft() { return time - System.currentTimeMillis(); }
+	
+	public boolean isReady() { return time - System.currentTimeMillis() - 1 <= 0; }
+	
+	
+	
+	public int compareTo(Object other) {
+		
+		Timer otherTimer = (Timer)other;
+		
+		if (time < otherTimer.time) return -1;
+		
+		else if (time == otherTimer.time) return 0;
+		
+		else return 1;
+		
+	}
+	
+	
+	
+	/**
+		 
+	*	Called by dispatcher thread to start event.
+	
+	*
+	
+	*/ 
+	
+	private void doEvent() {
+		
+		if (runAsThread) {
+			
+			(new Thread(runnable)).start();
+			
+		} else {
+			
+			try {
 				
-
-				thread.start();
-
+				runnable.run();
+				
+			} catch (Exception ex) {
+				
+				ex.printStackTrace();
+				
 			}
-
+			
 		}
-
+		
+	}
+	
+	
+	
+	
+	
+	static private Semaphore semaphore = new Semaphore(0);
+	
+	static private MinHeap timers = new MinHeap();
+	
+	static private Thread thread;
+	
+	
+	
+	static private void addEvent(Timer timer) {
+		
+		synchronized (timers) {
+			
+			timers.add(timer);
+			
+			timers.notify(); //instead of sem.getLock(time);
+			
+			
+			
+			if (thread == null)  { 
+				
+				thread = (new Thread() {
+					
+					public void run() {
+						
+						try {timerLoop();} catch (Exception ex) { ex.printStackTrace(); }
+						
+					}
+					
+				});
+				
+				
+				
+				thread.start();
+				
+			}
+			
+		}
+		
 	}
 
 	
@@ -233,53 +233,53 @@ public class Timer implements Comparable {
 
 
 	static private void timerLoop() {
-
+		
 		for(;;) {
-
+			
 			Timer timer = null;
-
+			
 			synchronized (timers) {
-
+				
 				boolean isEmpty = false;
-
+				
 				long timeLeft = 0;
-
-
-
-                if (timers.isEmpty()) isEmpty = true;
-
-                else timeLeft = ((Timer)timers.top()).timeLeft();
-
-
-
+				
+				
+				
+				if (timers.isEmpty()) isEmpty = true;
+				
+				else timeLeft = ((Timer)timers.top()).timeLeft();
+				
+				
+				
 				try {
-
+					
 					if (isEmpty) timers.wait();
-
+					
 					else if (timeLeft > 0) timers.wait(timeLeft);
-
+					
 				} catch(InterruptedException e) {
-
+					
 					throw new UnexpectedInterrupt(e.getMessage()); 
-
+					
 				}
-
-			
-
-                if (!timers.isEmpty() && ((Timer)timers.top()).isReady())
-
-                    	timer = (Timer)timers.extractTop();
-
+				
+				
+				
+				if (!timers.isEmpty() && ((Timer)timers.top()).isReady())
+					
+					timer = (Timer)timers.extractTop();
+				
 			}
-
 			
-
+			
+			
 			if (timer != null) //should never happen that timer==null;
-
-					timer.doEvent();
-
+				
+				timer.doEvent();
+			
 		}
-
+		
 	}
 
 }
