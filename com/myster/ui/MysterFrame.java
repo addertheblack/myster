30ba6d3fc06eb1ff449d46242215d2cf1d03a7fe
addertheblack <>
2002-10-26 17:10:46 +0000

    gf

---
+++ com/myster/ui/MysterFrame.java
@@ -0,0 +1,110 @@
+package com.myster.ui;
+
+import java.awt.Frame;
+
+import java.awt.event.WindowListener;
+import java.awt.event.WindowEvent;
+
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+
+import java.awt.Point;
+
+public class MysterFrame extends Frame {	
+	static int xStart=5;
+	static int yStart=5;
+
+	public MysterFrame() {
+		super();//explicit good
+		
+		initEvents();
+	}
+	
+	public MysterFrame(String windowName) {
+		super(windowName);
+		
+		initEvents();
+	}
+	
+	private static synchronized Point getWindowStartingLocation() {
+		Point location=new Point(xStart, yStart);
+		xStart+=20;
+		yStart+=20;
+		
+		if (xStart>250) {
+			xStart=0;
+			yStart=0;
+		}
+		
+		return location;
+	}
+	
+	private void initEvents() {
+		setLocation(getWindowStartingLocation());
+	
+		addWindowListener(new WindowListener() {	//inline class
+		
+			public void windowOpened(WindowEvent e) {
+
+			}
+
+			public void windowClosing(WindowEvent e) {
+				//cleanup goes here
+				WindowManager.removeWindow(MysterFrame.this);
+			}
+
+			public void windowClosed(WindowEvent e) {
+
+			}
+
+			public void windowIconified(WindowEvent e) {
+			
+			}
+
+
+			public void windowDeiconified(WindowEvent e) {
+			
+			}
+
+
+			public void windowActivated(WindowEvent e) {
+				WindowManager.setFrontWindow(MysterFrame.this);
+			}
+
+			public void windowDeactivated(WindowEvent e) {
+				
+			}
+		});
+		
+		addComponentListener(new ComponentListener() {
+		
+			public void componentResized(ComponentEvent e) {
+			
+			}
+
+			public void componentMoved(ComponentEvent e) {
+			
+			}
+
+			public void componentShown(ComponentEvent e) {
+				WindowManager.addWindow(MysterFrame.this);
+			}
+
+			public void componentHidden(ComponentEvent e) {
+				WindowManager.removeWindow(MysterFrame.this);
+			}
+		
+		});
+		
+		com.myster.menubar.MysterMenuBar.addMenuListener(new com.myster.menubar.event.MenuBarListener() {
+			public void stateChanged(com.myster.menubar.event.MenuBarEvent e) {
+				setMenuBar(e.makeNewMenuBar());
+			}
+		});
+	
+	}
+	
+	public void closeWindowEvent() {
+		processWindowEvent(new WindowEvent(this,WindowEvent.WINDOW_CLOSING));
+	}
+}
