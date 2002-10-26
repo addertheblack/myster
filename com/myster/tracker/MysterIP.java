0c8c91f1cbb208d02394117c2a9715e37410b4a7
addertheblack <>
2002-10-26 16:48:35 +0000

    small change.

--- com/myster/tracker/MysterIP.java
+++ com/myster/tracker/MysterIP.java
@@ -184,7 +184,7 @@ class MysterIP {
 		}
 		
 		public String getServerIdentity() {
-			return (serverIdentity==null?""+ip:(serverIdentity.length()>31?serverIdentity.substring(0,31):serverIdentity));
+			return (serverIdentity==null?""+ip:serverIdentity);//(serverIdentity.length()>31?serverIdentity.substring(0,31):serverIdentity));
 		}	
 		
 		public int getPingTime() {
