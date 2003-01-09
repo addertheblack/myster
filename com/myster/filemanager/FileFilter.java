4a53192e344e98436379f6c43d2c18516477d767
addertheblack <>
2003-01-09 16:14:17 +0000

    ROMS type added.

--- com/myster/filemanager/FileFilter.java
+++ com/myster/filemanager/FileFilter.java
@@ -10,6 +10,7 @@ class FileFilter {
 	private static final int PICT=3;
 	private static final int MACS=4;
 	private static final int WINT=5;
+	private static final int ROMS=6;
 	
 	private static String[][] fileextensions=new String[][]{
 		new String[]{
@@ -24,23 +25,24 @@ class FileFilter {
                         ".wm",".vob",".ifo"
                 },
 		new String[]{
-			".txt",".doc",".abw",".rtf",".pdf",".ps",".htm",
-			".html",".xml",".wp4"
+						".txt",".doc",".abw",".rtf",".pdf",".ps",".htm",
+						".html",".xml",".wp4"
 		},
 		new String[]{
                         ".jpg",".jpeg",".png",".gif",".jpe",".tif",".tiff",".bmp",
                         ".jp2",".ps",".psp",".eps",".xpm",".tga",".xcf",".pict"
 		},
 		new String[]{
-			".bin",".hqx",".sit",".cpt",".dmg",".sea",".nfs",".sitx",
+						".bin",".hqx",".sit",".cpt",".dmg",".sea",".nfs",".sitx",
                         ".zip",".gz",".tar",".tgz",".image",".img",".pkg",".smi",
-                        ".rom",".smc",".n64",".v64",".gba",".gb",".mod",".img"
 		},
 		new String[]{
                         ".exe",".zip",".gz",".tar",".tgz",".z",".rmj",".lqt",".iso",
-                        ".rpm",".cue",".iso",".bin",".com",".deb",".rom",".smc",".n64",
-                        ".v64",".gba",".gb",".mod",".7z",".fcd",".ccd",".rar",".ace",
+                        ".cue",".iso",".ccd",".rar",".ace",
                         ".cdr",".gzip",".lzh",".lha"
+		},
+		new String[]{
+                        ".zip","rom","smc","n64","v64","gba","gb","mod","img"
 		}
 	};
 	
@@ -59,6 +61,8 @@ class FileFilter {
 			return getThingy(MACS, file);
 		} else if (type.equals("WINT")) {
 			return getThingy(WINT, file);
+		} else if (type.equals("ROMS")) {
+			return getThingy(ROMS, file);
 		}
 		
 		return !file.getName().endsWith(".i");
