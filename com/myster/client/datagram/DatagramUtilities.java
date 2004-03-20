e0460cd491ed303ab3fe57dd68c90579305ab813
addertheblack <>
2004-03-20 22:34:39 +0000

    Minor typos and other fixes.

--- com/myster/client/datagram/DatagramUtilities.java
+++ com/myster/client/datagram/DatagramUtilities.java
@@ -7,13 +7,15 @@ import com.myster.net.StandardDatagramEvent;
 public class DatagramUtilities {
 	public static boolean dealWithError(Transaction transaction, StandardDatagramListener listener) {
 		if (transaction.isError()) {
-			listener.error(new StandardDatagramEvent(transaction.getAddress(),
-													transaction.getTransactionCode(),
-													new Integer(transaction.getErrorCode())));
-													
-			return true;
+			//This NORMALLY means that the protocol was not understood.
+			//Implementors *can* assume that this is the error without
+			//checking.
+			listener.error(new StandardDatagramEvent(
+					transaction.getAddress(),
+					transaction.getTransactionCode(),
+					new Integer(transaction.getErrorCode())));
+			return true;// there was an "error"
 		}
-		
 		return false;
 	}
 }
--- com/myster/net/StandardDatagramListener.java
+++ com/myster/net/StandardDatagramListener.java
@@ -1,8 +1,21 @@
 package com.myster.net;
 
 public interface StandardDatagramListener {
+
+	/**
+	*	This is called if there was no error
+	*/
 	public void response(StandardDatagramEvent event);
+	
+	/**
+	*	This is called if there was a timeout
+	*/
 	public void timeout(StandardDatagramEvent event);
+	
+	/**
+	*	This is calle dif there was a negative responce
+	*	(This can be assume to mean the protocol was not understood)
+	*/
 	public void error(StandardDatagramEvent event);
 }
 
