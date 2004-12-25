32ef79f9d80f48fea23a7e43f8433749137d8b6d
addertheblack <>
2004-12-25 17:17:46 +0000

    Searches now stop when the seaqrch window is closed.

--- com/myster/search/ui/SearchWindow.java
+++ com/myster/search/ui/SearchWindow.java
@@ -24,6 +24,8 @@ import java.awt.Rectangle;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import com.general.mclist.MCList;
 import com.general.mclist.MCListEvent;
@@ -148,6 +150,11 @@ public class SearchWindow extends MysterFrame implements SearchResultListener, S
         });
 
         addWindowListener(new StandardWindowBehavior());
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                stopSearch();
+            }
+        });
 
         //bucket=new SearchResultBucket(BUCKETSIZE);
 
