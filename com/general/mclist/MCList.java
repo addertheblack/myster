2492c4f9acf58297dc7f48dfb2392b68a0723e29
addertheblack <>
2005-03-26 21:09:16 +0000

    New interface for the MCList

---
+++ com/general/mclist/MCList.java
@@ -0,0 +1,40 @@
+
+package com.general.jmclist;
+
+import java.awt.Component;
+import java.awt.Font;
+
+import com.general.mclist.MCListEventListener;
+import com.general.mclist.MCListItemInterface;
+
+public interface MCList {
+    public void setNumberOfColumns(int c);
+    public void setColumnName(int columnnumber, String name);
+    public void setColumnWidth(int index, int size);
+    public void sortBy(int column);
+    public Component getPane();
+    public void addItem(MCListItemInterface m);
+    public void addItem(MCListItemInterface[] m);
+    public void setSorted(boolean isSorted);
+    public boolean isSorted() ;
+    public boolean isSelected(int i);
+    public void select(int i);
+    public void unselect(int i);
+    public void clearAllSelected();
+    public void toggle(int i);
+    public boolean isAnythingSelected();
+    public int[] getSelectedIndexes();
+    public int getSelectedIndex();
+    public void setSingleSelect(boolean b);
+    public boolean isSingleSelect();
+    public void addMCListEventListener(MCListEventListener e);
+    public void clearAll();
+    public void removeItem(int i);
+    public void removeItem(MCListItemInterface o);
+    public void removeItem(int[] indexes);
+    public Object getItem(int i);
+    public MCListItemInterface getMCListItem(int i);
+    public void reverseSortOrder();
+    public int length();
+    public Font getFont();
+}
